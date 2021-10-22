1d88ec0201862c530988a5c506c0c7a22f5a179d
EllaSushytskaya <ella.sushytskaya@gmail.com>
2021-10-22 19:01:09 +0300

    Add MainGoogle.java

--- src/main/java/lesson4/MainGoogle.java
+++ src/main/java/lesson4/MainGoogle.java
@@ -6,33 +6,44 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 
 public class MainGoogle {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
 
 		WebElement searchField = driver.findElement(By.name("q"));
 
-		searchField.sendKeys("Banana");
+		searchField.sendKeys("youtube");
 		searchField.sendKeys(Keys.ENTER);
 
-		List<WebElement> searchResults =driver.findElements(By.tagName("h3"));
-		WebElement firsElement =searchResults.get(1);
+		List<WebElement> searchResults = driver.findElements(By.tagName("mouseover-overlay"));
+		WebElement firsElement = searchResults.get(0);
 		firsElement.click();
 
-		driver.navigate().refresh();
 
-		new WebDriverWait(driver, Duration.ofSeconds(4))
-				.withMessage("Not search result")
-				.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h3")));
+		WebElement searchField2 = driver.findElement(By.name("search_query"));
+		searchField2.sendKeys("banana");
+		searchField2.sendKeys(Keys.ENTER);
 
+
+
+		WebElement searchVideo = driver.findElement(By.tagName("ytd-moving-thumbnail-renderer[@class=\"style-scope ytd-thumbnail\"]"));
+		searchVideo.click();
+
+
+		Thread.sleep(1000);
+
+
+//		driver.navigate().refresh();
+//
+//		new WebDriverWait(driver, Duration.ofSeconds(4))
+//				.withMessage("Not search result")
+//				.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h3")));
 
 
 		driver.close();
