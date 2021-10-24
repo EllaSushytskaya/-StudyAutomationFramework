813ef2d6c67f72333bbd0025ead0d699bb5ede49
EllaSushytskaya <ella.sushytskaya@gmail.com>
2021-10-24 22:04:36 +0300

    Add Lesson5

--- src/test/java/com/stormnet/lesson5/tests/AbstractTest.java
+++ src/test/java/com/stormnet/lesson5/tests/AbstractTest.java
@@ -1,10 +1,12 @@
 package com.stormnet.lesson5.tests;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 
 public class AbstractTest {
 	protected WebDriver driver;
--- src/test/java/com/stormnet/lesson5/tests/OpenThirdElementTest.java
+++ src/test/java/com/stormnet/lesson5/tests/OpenThirdElementTest.java
@@ -1,6 +1,5 @@
 package com.stormnet.lesson5.tests;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -8,14 +7,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.List;
 
-public class OpenThirdPageTest extends AbstractTest {
+public class OpenThirdElementTest extends AbstractTest {
 
-	@Test(testName = "OpenThirdPageTest")
+	@Test(testName = "OpenThirdElementTest")
 
-	public void openThirdPageTest(){
+	public void openThirdElementTest(){
 	driver.get("https://www.google.com/");
 
 		WebElement searchField = driver.findElement(By.name("q"));
@@ -35,7 +33,6 @@ public class OpenThirdPageTest extends AbstractTest {
 
 
 		Assert.assertTrue(thirdElement.isDisplayed());
-
 
 
 	}
